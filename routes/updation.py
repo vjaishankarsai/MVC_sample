932f56bf3eeb99ce0587b573904e36e7aef948e0
vjaishankarsai <49426491+vjaishankarsai@users.noreply.github.com>
2019-05-27 14:46:31 +0530

    second

--- routes/updation.py
+++ routes/updation.py
@@ -3,15 +3,16 @@ from flask import request
 from flask import jsonify
 from controller.UpdationController import UpdationController
 
-
+#to create an object for blueprint
 bp = Blueprint(__name__, "updation")
 
+#to create an object for updation controller
 icc = UpdationController()
 
 @bp.route("/updation", methods=["POST"])
 def insertion():
-    data = request.get_json()
+    data = request.get_json()                           #to get data
     name = data["name"]
     address = data["address"]
-    update_result = icc.update(name, address)
-    return jsonify(update_result)
+    update_result = icc.update(name, address)           #call update method in controller
+    return jsonify(update_result)                       #to return the result in the json format 
