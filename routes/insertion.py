c0c1216319ae5028fea37f2e01d76b0c0122b522
vjaishankarsai <49426491+vjaishankarsai@users.noreply.github.com>
2019-05-27 14:46:09 +0530

    second

--- routes/insertion.py
+++ routes/insertion.py
@@ -3,15 +3,16 @@ from flask import request
 from flask import jsonify
 from controller.InsertionController import InsertionController
 
-
+#to create an object for blueprint
 bp = Blueprint(__name__, "insertion")
 
+#to create an object for insertion controller
 icc = InsertionController()
 
 @bp.route("/insertion", methods=["POST"])
 def insertion():
-    data = request.get_json()
+    data = request.get_json()                           #to get data
     name = data["name"]
     address = data["address"]
-    ins_result = icc.insert(name, address)
-    return jsonify(ins_result)
+    ins_result = icc.insert(name, address)              #call insert method in controller
+    return jsonify(ins_result)                          #to return the result in the json format 
