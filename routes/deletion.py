5bce0026df971ed80f977ec384699edc019501d0
vjaishankarsai <49426491+vjaishankarsai@users.noreply.github.com>
2019-05-27 14:45:45 +0530

    second

--- routes/deletion.py
+++ routes/deletion.py
@@ -3,14 +3,15 @@ from flask import request
 from flask import jsonify
 from controller.DeletionController import DeletionController
 
-
+#to create an object for blueprint
 bp = Blueprint(__name__, "deletion")
 
+#to create an object for deletion controller
 icc = DeletionController()
 
 @bp.route("/deletion", methods=["POST"])
 def deletion():
-    data = request.get_json()
-    name = data["name"]
-    del_result = icc.remove(name)
-    return jsonify(del_result)
+    data = request.get_json()                   #to get data                   
+    name = data["name"] 
+    del_result = icc.remove(name)               #call remove method in controller
+    return jsonify(del_result)                  #to return the result in the json format    
