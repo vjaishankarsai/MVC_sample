41df6f713c0658ba95cce0df00b04ea354799e99
vjaishankarsai <49426491+vjaishankarsai@users.noreply.github.com>
2019-05-27 14:36:07 +0530

    second

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,3 +1,6 @@
 from . import InsertionController
+from . import DeletionController
+from . import UpdationController
+from . import Common
 
-__all__ = [InsertionController]
+__all__ = [InsertionController, DeletionController, UpdationController, Common]
